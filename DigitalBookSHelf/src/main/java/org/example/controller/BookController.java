593f017dd4dae5db944816affbb71a796b698177
harik-kdu <hari.k@kickdrum.com>
2026-01-08 15:59:08 +0530

    excercise 02

--- DigitalBookSHelf/src/main/java/org/example/controller/BookController.java
+++ DigitalBookSHelf/src/main/java/org/example/controller/BookController.java
@@ -1,9 +1,12 @@
 package org.example.controller;
 
 import org.example.entity.Book;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -24,8 +27,44 @@ public class BookController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Book>> getAllBooks() {
-        return ResponseEntity.ok(new ArrayList<>(bookshelf.values()));
+    public ResponseEntity<List<Book>> getAllBooks(
+        @RequestParam(name="author", required = false, defaultValue = "") String author,
+        @RequestParam(name="sort", required = false, defaultValue = "asc") String sort,
+        @RequestParam(name="pages", required = false, defaultValue = "1")  int page
+        ) 
+    {
+       
+        List<Book> result = new ArrayList<>(bookshelf.values());
+
+        if(!author.isEmpty()){
+            result = result.stream().filter((b)->
+            b.getAuthor().toLowerCase().equals(author.toLowerCase())
+        ).toList();
+        }
+
+        if (sort.equalsIgnoreCase("desc")) {
+            result = result.stream()
+                    .sorted(Comparator.comparing(Book::getTitle).reversed())
+                    .toList();
+        } 
+        else {
+            result = result.stream()
+                    .sorted(Comparator.comparing(Book::getTitle))
+                    .toList();
+        }
+
+        int pageSize = 10;
+        int start = (page - 1) * pageSize;
+
+        if (start >= result.size()) {
+            return ResponseEntity.ok(List.of());
+        }
+
+        int end = Math.min(start + pageSize, result.size());
+        List<Book> pagedResult = result.subList(start, end);
+
+        return ResponseEntity.ok(pagedResult);
+         
     }
 
     @PutMapping("/{id}")
@@ -49,5 +88,21 @@ public class BookController {
 
         bookshelf.remove(id);
         return ResponseEntity.noContent().build();  
+    }
+
+
+    @GetMapping("/{id}")
+    public EntityModel<Book> getBookById(@PathVariable Long id){
+        
+        Book book = bookshelf.get(id);
+        if (book == null) {
+            throw new NoSuchElementException("Book not found");
+        }
+
+        return EntityModel.of(
+                book,
+                linkTo(methodOn(BookController.class).getBookById(id)).withSelfRel(),
+                linkTo(methodOn(BookController.class).getAllBooks("", "asc", 1)).withRel("all-books")
+        );
     }
 }
